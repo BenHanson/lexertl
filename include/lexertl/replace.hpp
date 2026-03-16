235d43c78898c14725345c01c433b67f98fe7456
BenHanson <jamin.hanson@gmail.com>
2026-03-16 13:00:16 +0000

    Corrected iterators

--- include/lexertl/replace.hpp
+++ include/lexertl/replace.hpp
@@ -193,7 +193,7 @@ namespace lexertl
     out_iter replace(out_iter out_, fwd_iter first_, fwd_iter second_,
         const basic_state_machine<char_type, id_type>& sm_,
         const std::map<id_type,
-        const char_type*(*)(const char_type*, const char_type*)> fmt_)
+            const char_type*(*)(fwd_iter, fwd_iter)> fmt_)
     {
         fwd_iter last_ = first_;
         lexertl::match_results<fwd_iter, id_type> results_(first_, second_);
@@ -264,9 +264,11 @@ namespace lexertl
                 std::basic_string<char_type, ftraits, falloc> >& fmt_)
     {
         std::basic_string<char_type, straits, salloc> ret_;
-        const char_type* last_ = s_.c_str();
-        lexertl::match_results<const char_type*, id_type>
-            results_(s_.c_str(), s_.c_str() + s_.size());
+        typename std::basic_string<char_type, straits, salloc>::
+            const_iterator last_ = s_.begin();
+        lexertl::match_results<typename std::basic_string<char_type, straits,
+            salloc>::const_iterator, id_type>
+            results_(s_.begin(), s_.end());
 
         // Lookahead
         lexertl::lookup(sm_, results_);
@@ -305,9 +307,10 @@ namespace lexertl
                         const_iterator&)>& fmt_)
     {
         std::basic_string<char_type, straits, salloc> ret_;
-        const char_type* last_ = s_.c_str();
-        lexertl::match_results<const char_type*, id_type>
-            results_(s_.c_str(), s_.c_str() + s_.size());
+        typename std::basic_string<char_type, straits, salloc>::
+            const_iterator last_ = s_.begin();
+        lexertl::match_results<typename std::basic_string<char_type, straits,
+            salloc>::const_iterator, id_type> results_(s_.begin(), s_.end());
 
         // Lookahead
         lexertl::lookup(sm_, results_);
